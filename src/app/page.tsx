53472c80895377a1e7ce7202f1122c5f35b6633c
OKhorshid <okhorshid.00@gmail.com>
2024-06-17 00:35:33 +0300

    add input ui & use it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-a7aaaaa
+      <form>
+        <Input type="url" placeholder="url" />
+        <button className="border-2">submit</button>
+      </form>
     </main>
   );
 }
