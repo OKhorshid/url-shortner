c49cf6e6cc9d09842019668f8d4ea94af90881d1
OKhorshid <okhorshid.00@gmail.com>
2024-06-17 01:16:04 +0300

    setup prisma & import in backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
+import { prisma } from "@/lib/prismaClient";
 
 export default function Home() {
+  async function createUrl(formData: FormData) {
+    "use server";
+    const rawFormData = {
+      longUrl: formData.get("longUrl") as string,
+    };
+    console.log(rawFormData.longUrl);
+    let x = Date.now().toString();
+    await prisma.url.create({
+      data: {
+        long: rawFormData.longUrl,
+        short: x,
+      },
+    });
+
+    // ...
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form>
-        <Input type="url" placeholder="url" />
+      <form action={createUrl}>
+        <input type="text" placeholder="url" name="longUrl" />
         <button className="border-2">submit</button>
       </form>
     </main>
